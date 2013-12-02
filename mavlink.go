8156b41d104d183df66db8c665b7ef6bebfe4db2
Erik Unger <erik@erikunger.com>
2013-12-02 15:52:28 +0100

    Receive()

--- mavlink.go
+++ mavlink.go
@@ -2,6 +2,7 @@ package mavlink
 
 import (
 	"encoding/binary"
+	"fmt"
 	"io"
 	// "io/ioutil"
 )
@@ -11,15 +12,19 @@ var (
 	sequenceCounter uint8
 )
 
-type checksumWriter struct {
+type checksum struct {
 	writer io.Writer
 	sum    uint16
 }
 
-func (self *checksumWriter) Write(data []byte) (n int, err error) {
+func (self *checksum) Write(data []byte) (n int, err error) {
 	// todo calc sum
 
-	return self.writer.Write(data)
+	if self.writer != nil {
+		return self.writer.Write(data)
+	} else {
+		return len(data), nil
+	}
 }
 
 func Send(writer io.Writer, systemID, componentID uint8, message Message) error {
@@ -27,7 +32,7 @@ func Send(writer io.Writer, systemID, componentID uint8, message Message) error 
 	if err != nil {
 		return err
 	}
-	checksum := checksumWriter{writer: writer}
+	checksum := checksum{writer: writer}
 
 	header := []byte{message.Size(), sequenceCounter, systemID, componentID, message.ID()}
 	_, err = checksum.Write(header)
@@ -43,7 +48,67 @@ func Send(writer io.Writer, systemID, componentID uint8, message Message) error 
 	return binary.Write(writer, binary.LittleEndian, checksum.sum)
 }
 
-func Receive(reader io.Reader) (msg Message, systemID, componentID uint8, err error) {
+type header struct {
+	PayloadLength  uint8
+	PacketSequence uint8
+	SystemID       uint8
+	ComponentID    uint8
+	MessageID      uint8
+}
 
-	return nil, 0, 0, nil
+type ErrUnknownMessageID uint8
+
+func (self ErrUnknownMessageID) Error() string {
+	return fmt.Sprintf("Unknow message ID %d", self)
+}
+
+type ErrInvalidChecksum uint16
+
+func (self ErrInvalidChecksum) Error() string {
+	return fmt.Sprintf("Invalid checksum %d", self)
+}
+
+func Receive(reader io.Reader) (msg Message, systemID, componentID uint8, err error) {
+	// Read first byte and check if it is 0xfe
+	var start [1]byte
+	_, err = reader.Read(start[:])
+	if err != nil {
+		return nil, 0, 0, err
+	}
+	if start[0] != 0xfe {
+		return nil, 0, 0, fmt.Errorf("Invalid start of frame %x", start)
+	}
+
+	// Create a tee to write all reads into checksum
+	var checksum checksum
+	teeReader := io.TeeReader(reader, &checksum)
+
+	var header header
+	err = binary.Read(teeReader, binary.LittleEndian, &header)
+	if err != nil {
+		return nil, 0, 0, err
+	}
+	if int(header.MessageID) >= len(messageFactory) {
+		// Unknow message type, read into dummy until end of message
+		dummy := make([]byte, header.PayloadLength+2)
+		reader.Read(dummy)
+		return nil, 0, 0, ErrUnknownMessageID(header.MessageID)
+	}
+
+	msg = messageFactory[header.MessageID]()
+	err = binary.Read(teeReader, binary.LittleEndian, msg)
+	if err != nil {
+		return nil, 0, 0, err
+	}
+
+	var streamChecksum uint16
+	err = binary.Read(reader, binary.LittleEndian, &streamChecksum)
+	if err != nil {
+		return nil, 0, 0, err
+	}
+	if streamChecksum != checksum.sum {
+		return nil, 0, 0, ErrInvalidChecksum(streamChecksum)
+	}
+
+	return msg, header.SystemID, header.ComponentID, nil
 }
